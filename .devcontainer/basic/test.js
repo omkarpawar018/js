9d1cc86beae3856dad33359c9ac978faa5472a4c
omkarpawar018 <110762365+omkarpawar018@users.noreply.github.com>
2024-02-06 13:07:21 +0000

    commit

--- .devcontainer/basic/test.js
+++ .devcontainer/basic/test.js
@@ -1,1 +1,23 @@
 console.log("Omkar" )
+console.log(3+3)
+
+const name="omkar"
+console.log(name);
+let age=21
+console.log(age);
+console.log(typeof name);
+console.log(typeof undefined);
+console.log(typeof null);
+
+let score="33abc"
+
+console.log(typeof (score));
+console.log(typeof score);
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+let somenumber=33;
+let stringNumber=String(somenumber)
+console.log(somenumber);
+console.log(typeof somenumber)
